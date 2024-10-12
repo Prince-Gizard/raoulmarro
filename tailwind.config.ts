22318f79585f13bd5dca9752cc76297365723781
BuraevVV <BuraevVV@mosreg.ru>
2024-10-12 13:39:33 +0300

    root settings for project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primaryMain: "oklch(0% 0 84.58)",
+      primaryLight: "oklch(36.16% 0.008 75.31)",
+      secondaryMain: "oklch(82.58% 0.018 81.32)",
+      secondaryLight: "oklch(99.06% 0.01352908677048263 92.97793627734326)",
+      error: "oklch(65.65% 0.23 39.43)",
+
+      // ...
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
